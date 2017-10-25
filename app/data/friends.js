23f46df731a6307759d05eab10dde585a7896311
samuelboediono <samuel_boediono@hotmail.com>
2017-10-25 16:00:25 -0700

    server home friends done

---
+++ app/data/friends.js
@@ -0,0 +1,41 @@
+var friendList = [
+  {
+    name: "Samuel Boediono",
+    profilePic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+
+  {
+    name: "Ahmed",
+    profilePic: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  }
+
+]
+
+module.exports = friendList;
+
+
+
